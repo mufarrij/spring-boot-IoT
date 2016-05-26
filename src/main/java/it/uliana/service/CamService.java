f66491d596b7c126ca0b8e717591e8c1ae063199
Francesco Uliana <francesco@uliana.it>
2016-05-26 20:49:26 +0200

    cam webcam-capture

--- src/main/java/it/uliana/service/CamService.java
+++ src/main/java/it/uliana/service/CamService.java
@@ -1,14 +1,15 @@
 package it.uliana.service;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.ByteArrayOutputStream;
+import com.github.sarxos.webcam.Webcam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Optional;
 
 /**
  * Created by Francesco Uliana <francesco@uliana.it> on 21/05/16.
@@ -18,18 +19,22 @@ public class CamService {
 
     private final static Logger logger = LoggerFactory.getLogger(CamService.class);
     public static final String FORMAT_NAME = "jpg";
-    private static final String file = "web-cam-shot.jpg";
 
     public byte[] snap() throws IOException {
+        logger.debug("opening webcam");
+        Webcam webcam = Optional
+                .ofNullable(Webcam.getDefault())
+                .orElseThrow(() -> new IOException("webcam is not present"));
 
-        Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("fswebcam -r 640x480 --jpeg 85 -D 1 " + file);
-        InputStream is = pr.getInputStream();
-        String s = IOUtils.toString(is);
-        FileInputStream fis = new FileInputStream(file);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        IOUtils.copy(fis, bos);
-        return bos.toByteArray();
+        logger.info("using webcam {} - {}", webcam.getName(), webcam.getDevice().getName());
+        webcam.open();
+        BufferedImage img = webcam.getImage();
+        webcam.close();
 
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        ImageIO.write(img, FORMAT_NAME, bao);
+        byte[] bytes = bao.toByteArray();
+        logger.info("created image {} of size {}", FORMAT_NAME, bytes.length);
+        return bytes;
     }
 }
